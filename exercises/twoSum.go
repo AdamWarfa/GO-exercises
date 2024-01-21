f600d29d4c6fcf348879b976a7ebc91aa88ddff8
AdamWarfa <Adamwarfadk@gmail.com>
2024-01-21 17:59:07 +0100

    Update twoSum.go

--- exercises/twoSum.go
+++ exercises/twoSum.go
@@ -1,16 +1,12 @@
 package exercises
 
-import "fmt"
-
-func TwoSum(nums []int, target int) []int {
-	result := []int{}
+func twoSum(nums []int, target int) []int {
 	for i := 0; i < len(nums); i++ {
 		for j := 0; j < len(nums); j++ {
 			if nums[i]+nums[j] == target && i != j {
-				result = []int{i, j}
+				return []int{i, j}
 			}
 		}
 	}
-	fmt.Println(result)
-	return result
+	return []int{}
 }
